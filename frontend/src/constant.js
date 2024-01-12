d5670da5e2a7c178161ff041fcd92f0d9a1664fe
Stephegbenga <adegbamiyestephen2018@gmail.com>
2024-01-12 01:19:34 +0100

    changed the url to live urls

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -2,8 +2,8 @@ const frontend_url = "https://vcard-g7jd.onrender.com"
 const base_url = "https://vcard-g7jd.onrender.com"
 
 
-// const frontend_url = "http://localhost:3000"
-// const base_url = "http://localhost:5000"
+// const frontend_url = "https://67b1-129-205-113-175.ngrok-free.app"
+// const base_url = "https://67b1-129-205-113-175.ngrok-free.app"
 
 
 export {frontend_url, base_url}
