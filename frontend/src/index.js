f8dba4ba69aa282bed070b38b95dba231e74507f
Stephegbenga <adegbamiyestephen2018@gmail.com>
2024-02-16 17:01:16 +0100

    added a home page as the same editor

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,6 +17,7 @@ const App = () => {
   return (
     <Router>
       <Switch>
+      <Route component={Editor} exact path="/" />
         <Route component={Editor} exact path="/editor" />
         <Route component={Vcards} exact path="/vcards" />
         <Route component={Profile} exact path="/:short_id" />
